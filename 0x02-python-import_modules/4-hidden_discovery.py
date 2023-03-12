3851c91e39dfb093e18b0434fa42006cd849095a
Nourhan <nourhan201717@yahoo.com>
2023-03-12 11:31:46 +0200

    done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     mylist = dir(hidden_4)
     newlist = sorted(mylist)
     while i < len(newlist):
-    if newlist[i][0] != '_':
-         print(newlist[i])
-     i += 1
+        if newlist[i][0] != '_':
+             print(newlist[i])
+         i += 1
 
