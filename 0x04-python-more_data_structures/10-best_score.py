3949177b0bbd83eb2ca7cff77564d17f9d49ac3c
Nourhan <nourhan201717@yahoo.com>
2023-03-15 11:15:39 +0200

    task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    x = 0
+    result = ""
+    if a_dectionary:
+        for k, v in a_dictionary.items():
+            if v > x
+                result = k
+                x = v
+            return result
+        else:
+            return None
